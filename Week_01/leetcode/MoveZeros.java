cb0cb2eacefb113e42eb8ca2feedde502037b56c
cychengming <cychengming@163.com>
2021-01-03 11:23:59 +0800

    add java file

---
+++ Week_01/leetcode/MoveZeros.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class MoveZeros {
+	public void moveZeroes(int[] nums) {
+        int j = 0;
+		for ( int i=0;i<nums.length;i++){
+			if (nums[i]!=0){
+				nums[j] = nums[i];
+                //相当于交换元素0及非零元素的位置
+				if (i!=j){
+					nums[i] = 0;
+				}				
+				j++;				
+			}		
+		}	 
+
+    }
+}
