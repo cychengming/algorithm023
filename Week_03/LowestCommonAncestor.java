f85fc1e57fa4532975a12d549443084013314e5b
cychengming <cychengming@163.com>
2021-01-17 22:47:29 +0800

    add the third week homework

---
+++ Week_03/LowestCommonAncestor.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class LowestCommonAncestor {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (null == root)
+			return root;
+		if (root == p || root == q)
+			return root;
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (left != null && right != null)
+			return root;
+		if (left != null)
+			return left;
+		if (right != null)
+			return right;
+		return null;
+	}
+
+}
